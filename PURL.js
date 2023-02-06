01ea9e05b1f43beccc3a426bf2a4cf644d762651
vv-1757 <76858419+vv-1757@users.noreply.github.com>
2023-02-06 12:43:06 +0530

    Create PURL.js

---
+++ PURL.js
@@ -0,0 +1,25 @@
+var SHINCHAN_MOVIES={
+"Mov":{
+"Mov1":
+{
+"Thumb":"https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie%20Thumbnails/Shinchan-Movie-1.jpg",
+"TI":"https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-13.png",
+"SS":["https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Compress/Movie-1-Screenshoot-1.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Compress/Movie-1-Screenshoot-2.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Compress/Movie-1-Screenshoot-3.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-4.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-5.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-6.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-7.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-8.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-9.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-10.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-12.png","https://vv-1757.github.io/Shinchan-Movies/Shinchan%20Movie-1/Movie-1-Screenshoot-12.png"],
+"Name":"Crayon Shin-chan: Action Mask vs. Leotard Devil",
+"Transcript":"&#x30AF&#x30EC&#x30E8&#x30F3&#x3057&#x3093&#x3061&#x3083&#x3093&#x20&#x30A2&#x30AF&#x30B7&#x30E7&#x30F3&#x4EEE&#x9762&#x5BFE&#x30CF&#x30A4&#x30B0&#x30EC&#x9B54&#x738B",
+"ORG_N":"Kureyon Shinchan: Akushon Kamen tai Haigure Maō",
+"RDIJ":"July 24,1993",
+"DUR":"1h:33m",
+"Prod":"Shin-El Animation",
+"BoxOff":"",
+"RIIN":"August 29,2010",
+"MIIN":"Shinchan In Action Kamen vs Higure Rakshas",
+"LeadChar":"Leotard Devil(Higure Rakshas) , Baron T-Back , Belly Band Stomache Ladies , Razaya Dan , Action Mask , Fake Action Mask , Ririko and Mimiko Sakura:Action Mask's Partners, DR.Kitakasukabe,Navi tricycle(Shiro),Touru Kazama,Nene Sakurada ,Maso Sato ,Bo-Chan, Principal,Ume Matsuzaka,Midori Yoshinaga,Misae,Hiroshi,Shinchan",
+"MSize":"426,770,432 bytes",
+"MRes":"1280x720",
+"MLang":"Tamil",
+"MSub":"Yes"
+},
+"Mov2":{}
+}
+}
